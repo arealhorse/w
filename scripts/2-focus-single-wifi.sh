0cfaf270f989133284e68e66804800ee123301cb
arealhorse <103963416+arealhorse@users.noreply.github.com>
2024-06-30 08:39:31 +0700

    Update 2-focus-single-wifi.sh

--- scripts/2-focus-single-wifi.sh
+++ scripts/2-focus-single-wifi.sh
@@ -6,5 +6,6 @@ echo "Channel:"
 read channel
 mkdir -p captured
 timestamp=$(date +"%Y-%m-%d---%H-%M-%S")
+formatted_bssid=$(echo "$bssid" | tr ':' '_')
 
-sudo airodump-ng -w "captured/$timestamp---$bssid" -c $channel --bssid $bssid wlan0
+sudo airodump-ng -w "captured/$timestamp---$formatted_bssid" -c $channel --bssid $bssid wlan0
